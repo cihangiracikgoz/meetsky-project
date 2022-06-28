d1dabf024b33a2dc5f4eec51ade16eede38ba1b7
cihangir <cihangiracikgoz01@gmail.com>
2022-06-28 12:08:17 +0100

    CukesRunner file created

---
+++ src/test/java/net/meetsky/runners/CukesRunner.java
@@ -0,0 +1,16 @@
+package net.meetsky.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/cucumber-report.html",
+        features = "src/test/resources/features",
+        glue = "net/meetsky/step_definitions",
+        dryRun = false
+)
+
+public class CukesRunner {
+}
